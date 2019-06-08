800e04960b056017bc66e5b6cbf57d2d2f59470a
tian <t18215595124@gmail.com>
2019-06-08 11:31:21 +0800

    Update transformer.py

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -114,8 +114,6 @@ class TransformerModel(FairseqEncoderDecoderModel):
         parser.add_argument('--save-attn',default=False, action='store_true',
                             help='if set, disables positional embeddings (outside self attention)')
         parser.add_argument('--save-attn-path',type=str)
-
-        parser.add_argument('--share-input-output-embed',default=False,action='store_true')
         # fmt: on
 
     @classmethod
@@ -349,11 +347,12 @@ class TransformerDecoder(FairseqIncrementalDecoder):
                 factor=args.adaptive_softmax_factor,
                 tie_proj=args.tie_adaptive_proj,
             )
-        '''
+        
         elif not self.share_input_output_embed:
             print('share-input-output-embed')
             self.embed_out = nn.Parameter(torch.Tensor(len(dictionary), self.output_embed_dim))
             nn.init.normal_(self.embed_out, mean=0, std=self.output_embed_dim ** -0.5)
+        
         '''
         print('self.share_input_output_embed')
         print(self.share_input_output_embed)
@@ -361,7 +360,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
             print('share-input-output-embed')
             self.embed_out = nn.Parameter(torch.Tensor(len(dictionary), self.output_embed_dim))
             nn.init.normal_(self.embed_out, mean=0, std=self.output_embed_dim ** -0.5)
-
+        '''
         self.register_buffer('version', torch.Tensor([2]))
         self.normalize = args.decoder_normalize_before and final_norm
         if self.normalize:
@@ -453,8 +452,10 @@ class TransformerDecoder(FairseqIncrementalDecoder):
         if self.adaptive_softmax is None:
             # project back to size of vocabulary
             if self.share_input_output_embed:
+                print('output_layer 1')
                 return F.linear(features, self.embed_tokens.weight)
             else:
+                print('output_layer 2')
                 return F.linear(features, self.embed_out)
         else:
             return features
